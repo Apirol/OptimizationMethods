d3ae33743ed616b86fce3029d5ac9fb3bc8a0bef
Redut Anatoly <56587883+Apirol@users.noreply.github.com>
2021-05-11 01:54:19 +0700

    Updated interval in gss

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import numpy as np
 import numpy.linalg as ln
-from method import search_minimal_segment, search_minimal_segment2, golden_ratio_2, golden_ratio
+from method import golden_ratio, search_minimal_segment
 import numdifftools as nd
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def function_alpha2(alpha, xk, Hk):
-    return function(xk + alpha * np.dot(Hk, -gradient(xk)))
-
-
-def Rosenbrock_gradient(X):
-    x, y = X
-    return np.array([2 * (x - 1) - 4 * 100 * x * (y - x**2), 2 * 100 * (y - x**2)])
-
-
-def fast_gradient_method(f, fprime, x0, maxiter=10000, epsi=1e-8):
+def fast_gradient_method(f, fprime, x0, maxiter=10000, epsi=1e-6):
     df = pd.DataFrame()
 
     fig_size = plt.rcParams["figure.figsize"]
@@ -23,10 +14,10 @@ def fast_gradient_method(f, fprime, x0, maxiter=10000, epsi=1e-8):
     fig_size[1] = 12
     plt.rcParams["figure.figsize"] = fig_size
 
-    X = np.arange(-5, 5, 0.10)
-    Y = np.arange(-5, 5, 0.10)
+    X = np.arange(-20, 20, 0.5)
+    Y = np.arange(-20, 20, 0.5)
     X, Y = np.meshgrid(X, Y)
-    plt.contour(X, Y, f([X, Y]), 140)
+    plt.contour(X, Y, f([X, Y]))
     plt.xlabel('Current X')
     plt.ylabel('Current Function')
 
@@ -35,10 +26,12 @@ def fast_gradient_method(f, fprime, x0, maxiter=10000, epsi=1e-8):
     iter_counter: int = 0
     xk = x0
     current_gradient = fprime(x0)
+    iter_counter_func = 0
 
     while iter_counter < maxiter:
         q = lambda alpha: f(xk - alpha * current_gradient)
-        coeff = golden_ratio(q, 0, 1, epsi)
+        coeff, iter_func = golden_ratio(q, 0, 10, epsi)
+        iter_counter_func += iter_func
         xNext = xk - coeff * current_gradient
 
         current_iter = [{'X': xk[0], 'Y': xk[1], 'F': f(xk), 'Lambda': coeff, 'DX': xNext[0] - xk[0],
@@ -51,27 +44,25 @@ def fast_gradient_method(f, fprime, x0, maxiter=10000, epsi=1e-8):
         current_gradient = fprime(xk)
         iter_counter += 1
 
-        if np.linalg.norm(current_gradient) < epsi:
+        if np.linalg.norm(current_gradient) <= epsi:
             plt.show()
             df.to_excel('Report.xlsx')
-            return xk, iter_counter
+            return xk, iter_counter, iter_counter_func
 
     plt.show()
     df.to_excel('Report.xlsx')
-    return xk, iter_counter
+    return xk, iter_counter, iter_counter_func
 
 
 
 
-def function(X):
+def function(x):
     #return x[0]**2 + x[1]**2
-    x, y = X
+    #x, y = X
     #return 10 * x**2 + y**2
-    return x**2-x*y+y**2+9*x-6*y+20
-    #return (1 - x) ** 2 + 100 * (y - x) ** 2
-    #return -3 / (1 + (x[0] - 2)**2 + (x[1] - 2)**2 / 4) - 2 / (1 + (x[0] - 2)**2 / 9 + (x[1] - 3)**2)
-
-
+    #return x**2-x*y+y**2+9*x-6*y+20
+    #return (1 - x) ** 2 + 100 * (y - x**2) ** 2
+    return -3 / (1 + (x[0] - 2)**2 + (x[1] - 2)**2 / 4) - 2 / (1 + (x[0] - 2)**2 / 9 + (x[1] - 3)**2)
 
 
 def gradient(x):
@@ -88,10 +79,10 @@ def second_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-4):
     fig_size[1] = 12
     plt.rcParams["figure.figsize"] = fig_size
 
-    X = np.arange(-5, 5, 0.10)
-    Y = np.arange(-5, 5, 0.10)
+    X = np.arange(-20, 20, 0.5)
+    Y = np.arange(-20, 20, 0.5)
     X, Y = np.meshgrid(X, Y)
-    plt.contour(X, Y, f([X, Y]), 140)
+    plt.contour(X, Y, f([X, Y]))
 
     plt.plot(x0[0], x0[1], 'ro')
 
@@ -103,11 +94,13 @@ def second_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-4):
     Hk = I
     xk = x0
     alpha_k = 0.005
+    iter_counter_func = 0
 
     while k < maxiter:
         pk = np.dot(Hk, current_gradient)
         q = lambda alpha: f(xk - alpha * pk)
-        alpha_k = golden_ratio_2(q, 0, 1, epsi)
+        alpha_k, iter_func = golden_ratio(q, 0, 10, epsi)
+        iter_counter_func += iter_func
 
         xkp1 = xk - alpha_k * pk
 
@@ -125,19 +118,23 @@ def second_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-4):
         k += 1
 
         temp = 1 / np.dot(yk[np.newaxis, :], sk)
-        Hk = Hk + ((np.dot(sk - np.dot(Hk, yk), sk[:np.newaxis])) * temp)
+
+        if k % 2 == 0:
+            Hk = np.eye(N, dtype=int)
+        else:
+            Hk = Hk + ((np.dot(sk - np.dot(Hk, yk), np.dot(yk, Hk).transpose())) * temp)
 
         if ln.norm(current_gradient) < epsi:
             df.to_excel("Report_Second_Pearson.xlsx")
             plt.show()
-            return xk, k
+            return xk, k, iter_counter_func
 
     df.to_excel("Report_Second_Pearson.xlsx")
     plt.show()
-    return xk, k
+    return xk, k, iter_counter_func
 
 
-def third_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-7):
+def third_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-4):
     df = pd.DataFrame()
 
     fig_size = plt.rcParams["figure.figsize"]
@@ -145,10 +142,10 @@ def third_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-7):
     fig_size[1] = 12
     plt.rcParams["figure.figsize"] = fig_size
 
-    X = np.arange(-5, 5, 0.10)
-    Y = np.arange(-5, 5, 0.10)
+    X = np.arange(-20, 20, 0.5)
+    Y = np.arange(-20, 20, 0.5)
     X, Y = np.meshgrid(X, Y)
-    plt.contour(X, Y, f([X, Y]), 140)
+    plt.contour(X, Y, f([X, Y]))
 
     plt.plot(x0[0], x0[1], 'ro')
 
@@ -160,11 +157,13 @@ def third_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-7):
     Hk = I
     xk = x0
     alpha_k = 0.005
+    iter_counter_func = 0
 
     while k < maxiter:
         pk = np.dot(Hk, current_gradient)
         q = lambda alpha: f(xk - alpha * pk)
-        alpha_k = golden_ratio_2(q, 0, 1, epsi)
+        alpha_k, iter_func = golden_ratio(q, 0, 10, epsi)
+        iter_counter_func += iter_func
         xkp1 = xk - alpha_k * pk
 
         current_iter = [{'X': xk[0], 'Y': xk[1], 'F': f(xk), 'Lambda': alpha_k, 'DX': xkp1[0] - xk[0],
@@ -182,19 +181,23 @@ def third_Pearson(f, fprime, x0, maxiter=10000, epsi=1e-7):
         k += 1
 
         temp = 1 / np.dot(np.dot(yk[np.newaxis, :], Hk), yk)
-        Hk = Hk + ((np.dot(sk - np.dot(Hk, yk), np.dot(yk, Hk).transpose())) * temp)
+
+        if k % 22 == 0:
+            Hk = np.eye(N, dtype=int)
+        else:
+            Hk = Hk + ((np.dot(sk - np.dot(Hk, yk), np.dot(yk, Hk).transpose())) * temp)
 
         if ln.norm(current_gradient) < epsi:
             df.to_excel("Report_Third_Pearson.xlsx")
             plt.show()
-            return xk, k
+            return xk, k, iter_counter_func
 
     df.to_excel("Report_Third_Pearson.xlsx")
     plt.show()
-    return xk, k
+    return xk, k, iter_counter_func
 
 
-xk, counter = fast_gradient_method(function, gradient, np.array([10, 10]))
-result, k = third_Pearson(function, gradient, np.array([10, 10]))
-result1, k2 = second_Pearson(function, gradient, np.array([10, 10]))
+xk, counter, iter_counter = fast_gradient_method(function, gradient, np.array([0, 0]), epsi=1e-5)
+result, k, third_Pearson_iter = third_Pearson(function, gradient, np.array([0, 0]), epsi=1e-5)
+result1, k2, second_Pearson_iter = second_Pearson(function, gradient, np.array([0, 0]), epsi=1e-5)
 print("Stop")
